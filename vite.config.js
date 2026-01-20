fd064d788b8cb1d43a848cbfaaedb0043413b5fb
Aslıhan <erdallasll@gmail.com>
2026-01-20 23:20:18 +0000

    Add multiple HTML entry points to Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/goit-js-hw-10/",
@@ -6,5 +7,13 @@ export default defineConfig({
   build: {
     outDir: "../docs",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        timer: resolve(__dirname, "src/01-timer.html"),
+        snackbar: resolve(__dirname, "src/02-snackbar.html"),
+      },
+    },
   },
 });
+
